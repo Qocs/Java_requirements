2a0aa44fe31343ef3d3beea784e4d7c0601bbefd
Qocs <ahdehfl1004@gmail.com>
2024-07-25 23:53:05 +0900

    [study/variable, operator]

---
+++ src/ex/var/Variable3.java
@@ -0,0 +1,14 @@
+package ex.var;
+
+public class Variable3 {
+
+    public static void main(String[] args) {
+
+        long longVar = 10000000000L;
+        boolean booleanVar = true;
+
+        System.out.println(longVar);
+        System.out.println(booleanVar);
+
+    }
+}
